14f81fe7b887a1da2c962694ed3b73e32b799baf
Esben <esbenkramer@gmail.com>
2013-12-14 22:45:45 +0100

    lavede en lille test-ting i flajolet-Martin. Husk at reverte.

--- MySQLTest/src/FlajoletMartin.java
+++ MySQLTest/src/FlajoletMartin.java
@@ -73,9 +73,16 @@ public class FlajoletMartin {
         out = Math.pow(2.0, r);
         System.out.println(q+" "+q2+" "+q3+" r: " +r);   
         
-        double bias = 0.31/(Math.pow(2, r)); //find the bias
         
-        double modifiedOut = out*(1-bias);
+        
+        //The next two lines find the bias and subtract it.
+        double bias = 0.31/(Math.pow(2, r)); //find the bias
+        //double modifiedOut = out*(1-bias);
+        
+       
+        
+        //TODO delete this test, or be happy if it works. Next line applies the bias to R instead.
+        double modifiedOut = Math.pow(2.0, r-bias);
         
         
         //System.out.println(out/0.77351); //deprecated
